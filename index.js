0b883dfdc6fd6b08b1c657542e5760ecb0e46c61
shinsehantan <1219ssm@gmail.com>
2021-02-04 17:24:46 +0900

    비밀 번호 암호화

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-  const user = new User(req.userInfo)
+  const user = new User(req.body)
   user.save((err, doc) =>{
     if(err) return res.json({ success: false, err})
     return res.status(200).json({
